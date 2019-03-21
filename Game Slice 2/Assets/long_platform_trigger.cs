b7bbc8aba3fb7426eac37027757cd20ff413bbf5
Dylberto <dylang565@gmail.com>
2019-03-21 21:42:21 +0000

    portals?

---
+++ Game Slice 2/Assets/long_platform_trigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class long_platform_trigger : MonoBehaviour
+{
+
+    public Animator platform1;
+    public Animator platform2;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            platform1.SetBool("activate", true);
+            platform2.SetBool("activate", true);
+
+        }
+    }
+
+}
